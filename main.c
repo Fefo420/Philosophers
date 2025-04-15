264166eaea3538d462b0bbb5ef106e8c861b8f50
Fefo420 <fileonar@student.42Heilbronn.de>
2025-04-15 23:10:51 +0200

    rm module

--- main.c
+++ main.c
@@ -12,8 +12,24 @@
 
 #include "philosophers.h"
 
-int	main(void)
+static int	arguments_check(int argc, char **argv)
 {
-	ft_putstr("Hello, World!\n");
+	if (argc < 5 || argc > 6)
+		return (error_msg("Invalid number of arguments..."), EXIT_FAILURE);
+	if (argc == 6 && ft_atoi(argv[5]) < 1)
+		return (IMMIDIATE_SUCCESS);
+	while (--argc > 0)
+		if (ft_isdigit(argv[argc]) == EXIT_FAILURE)
+			return (error_msg("Arguments must be integers..."), EXIT_FAILURE);
+	if (ft_atoi(argv[1]) < 1 || ft_atoi(argv[1]) > 200)
+		return (EXIT_FAILURE);
+	if (ft_atoi(argv[2]) < 60 || ft_atoi(argv[3]) < 60 || ft_atoi(argv[4]) < 60)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+int	main(int argc, char **argv)
+{
+	arguments_check(argc, argv);
 	return (0);
 }
