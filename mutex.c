8be530c6ee81ebcf23d25172b81786df7def61e1
Fefo420 <fileonar@student.42Heilbronn.de>
2025-05-09 00:27:14 +0200

    fixed threads

--- mutex.c
+++ mutex.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   mutex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fileonar <fileonar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fefo <fefo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 22:09:49 by fileonar          #+#    #+#             */
-/*   Updated: 2025/05/07 19:12:00 by fileonar         ###   ########.fr       */
+/*   Updated: 2025/05/09 00:26:31 by fefo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	threads_checker(t_philo *philo_data, t_state_data *sdata)
+int	threads_maker(t_philo *philo_data, t_state_data *sdata)
 {
 	int	i;
 	int	stop;
@@ -24,10 +24,11 @@ int	threads_checker(t_philo *philo_data, t_state_data *sdata)
 	{
 		philo_data = &sdata->philos[i];
 		pthread_mutex_lock(&sdata->mutex_dead);
+		stop = sdata->dead;
+		pthread_mutex_unlock(&sdata->mutex_dead);
 		if (stop)
 			break ;
-		if (pthread_create(&philo_data->thread, NULL,
-				&sdata->philos, philo_data))
+		if (pthread_create(&philo_data->thread, NULL, &philo, philo_data))
 		{
 			error_msg("Something went wrong with create threads...\n");
 			break ;
@@ -36,4 +37,5 @@ int	threads_checker(t_philo *philo_data, t_state_data *sdata)
 		i++;
 	}
 	return (threads_made);
+
 }
