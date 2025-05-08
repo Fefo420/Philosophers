9b35eb2149e297652b5250b85df410fd5cf26fd9
Fefo420 <fileonar@student.42Heilbronn.de>
2025-05-08 23:18:04 +0200

    added set death

--- philosophers.c
+++ philosophers.c
@@ -3,14 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fileonar <fileonar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fefo <fefo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 22:03:45 by fileonar          #+#    #+#             */
-/*   Updated: 2025/05/07 22:06:44 by fileonar         ###   ########.fr       */
+/*   Updated: 2025/05/08 03:12:38 by fefo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+//SEND THIS TOMORROW
+int		set_death(t_state_data *sdata, int i)
+{
+	long	now;
+
+	pthread_mutex_lock(&sdata->mutex);
+	pthread_mutex_lock(&sdata->mutex_eating);
+	now = get_current_time();
+	if (now - sdata->philos[i].last_eat >= sdata->death_timer
+		&& sdata->philos[i].state != EATING)
+	{
+		pthread_mutex_lock(&sdata->mutex_dead);
+		sdata->dead = 1;
+		pthread_mutex_unlock(&sdata->mutex_dead);
+		sdata->philos[i].state = DEAD;
+		pthread_mutex_unlock(&sdata->mutex_eating);
+		pthread_mutex_unlock(&sdata->mutex);
+		pthread_mutex_lock(&sdata->msg);
+		printf("%ld %d died\n", get_current_time() - sdata->philos[i].start,
+			sdata->philos[i].id);
+		pthread_mutex_unlock(&sdata->msg);
+		return (1);
+	}
+	pthread_mutex_unlock(&sdata->mutex_eating);
+	pthread_mutex_unlock(&sdata->mutex);
+	return (0);
+
+}
 
 void	*philosophizer(void *philo)
 {
