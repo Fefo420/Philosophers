2d4c405d0a6e8e846387fc40e9a52ed4058afe0c
Fefo420 <fileonar@student.42Heilbronn.de>
2025-05-09 00:31:01 +0200

    philosophze

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fefo <fefo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 19:21:43 by fileonar          #+#    #+#             */
-/*   Updated: 2025/05/08 03:09:05 by fefo             ###   ########.fr       */
+/*   Updated: 2025/05/09 00:26:55 by fefo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,43 @@ static int	arguments_check(int argc, char **argv)
 	if (argc == 6 && ft_atoi(argv[5]) < 1)
 		return (EXIT_SUCCESS);
 	while (--argc > 0)
+	{
 		if (ft_isdigit(argv[argc]) == EXIT_FAILURE)
 			return (error_msg("Arguments must be integers..."), EXIT_FAILURE);
+	}
 	if (ft_atoi(argv[1]) < 1 || ft_atoi(argv[1]) > 200)
 		return (EXIT_FAILURE);
 	if (ft_atoi(argv[2]) < 60 || ft_atoi(argv[3]) < 60 || ft_atoi(argv[4]) < 60)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
+}
+
+static int	philosophize(t_state_data *sdata)
+{
+	t_philo		*philo_sdata;
+	pthread_t	monitor_thread;
+	int			threads_made;
+	int			i;
+
+	i = 0;
+	threads_made = 0;
+	philo_sdata = sdata->philos;
+	if (pthread_create(&monitor_thread, NULL, &routine_checker, sdata))
+		return (error_msg("Something went wrong with monitoring thread...\n"),
+			EXIT_FAILURE);
+	threads_made = threads_maker(philo_sdata, sdata);
+	while (i < threads_made)
+	{
+		philo_sdata = &sdata->philos[i];
+		if (pthread_join(philo_sdata->thread, NULL))
+			return (error_msg("Something went wrong with thread join...\n"),
+				EXIT_FAILURE);
+		i++;
+	}
+	if (pthread_join(monitor_thread, NULL))
+		return (error_msg("Something went wrong with monitor thread\n"),
+			EXIT_FAILURE);
+	return (free_and_exit(sdata), EXIT_SUCCESS);
 }
 
 int	main(int argc, char **argv)
@@ -41,7 +71,7 @@ int	main(int argc, char **argv)
 	if (init_thread(&sdata, argc, argv) == EXIT_FAILURE)
 		return (error_msg("Something went wrong when initializing thread..."),
 			EXIT_FAILURE);
-	if (philosophizer(&sdata))
+	if (philosophize(&sdata))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 	return (0);
